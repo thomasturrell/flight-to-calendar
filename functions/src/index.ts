6e2acb0acd137e74d9ff34cdff0151c8fcb5cd24
Thomas Turrell-Croft <1552612+thomasturrell@users.noreply.github.com>
2025-04-28 11:41:42 +0100

    wip

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,27 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import fetch from "node-fetch";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+export const apiProxy = functions.https.onRequest(async (req, res) => {
+  const API_KEY = process.env.API_TOKEN;
+  const endpoint = req.url;
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+  try {
+    const apiRes = await fetch(
+      `https://aerodatabox.p.rapidapi.com${endpoint}`,
+      {
+        method: req.method,
+        headers: {
+          "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
+          "x-rapidapi-key": API_KEY!,
+        },
+      }
+    );
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    const data = await apiRes.text();
+    res.setHeader("Content-Type", "application/json");
+    res.status(apiRes.status).send(data);
+  } catch (err: any) {
+    console.error("API Proxy error:", err);
+    res.status(500).send({ error: "API proxy failed" });
+  }
+});
