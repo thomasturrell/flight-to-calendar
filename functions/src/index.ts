3c6b67820918d50e53c830db88d6b6317d10b431
Thomas Turrell-Croft <1552612+thomasturrell@users.noreply.github.com>
2025-04-28 22:02:24 +0100

    wip

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,22 @@
+import axios from "axios";
 import * as functions from "firebase-functions";
-import fetch from "node-fetch";
 
 export const apiProxy = functions.https.onRequest(async (req, res) => {
   const API_KEY = process.env.API_TOKEN;
   const endpoint = req.url;
 
   try {
-    const apiRes = await fetch(
-      `https://aerodatabox.p.rapidapi.com${endpoint}`,
-      {
-        method: req.method,
-        headers: {
-          "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
-          "x-rapidapi-key": API_KEY!,
-        },
-      }
-    );
+    const apiRes = await axios({
+      url: `https://aerodatabox.p.rapidapi.com${endpoint}`,
+      method: req.method as any,
+      headers: {
+        "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
+        "x-rapidapi-key": API_KEY!,
+      },
+    });
 
-    const data = await apiRes.text();
     res.setHeader("Content-Type", "application/json");
-    res.status(apiRes.status).send(data);
+    res.status(apiRes.status).send(apiRes.data);
   } catch (err: any) {
     console.error("API Proxy error:", err);
     res.status(500).send({ error: "API proxy failed" });
